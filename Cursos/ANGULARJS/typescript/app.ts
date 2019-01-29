fae49916d0912f8c1898594773c16d1306b88629
caiooliveira54 <cdom.snf16@uea.edu.br>
2019-01-29 17:20:23 -0400

    add: Curso de Angularjs

---
+++ Cursos/ANGULARJS/typescript/app.ts
@@ -0,0 +1,7 @@
+let message: string = "Hello World!"
+console.log(message);
+
+let episode: number = 4
+console.log("This is episode " + episode)
+episode += 1;
+console.log("Next episode is" + episode)
---
+++ Cursos/ANGULARJS/typescript/dist/app.js
@@ -0,0 +1,6 @@
+var message = "Hello World!";
+console.log(message);
+var episode = 4;
+console.log("This is episode " + episode);
+episode += 1;
+console.log("Next episode is" + episode);
