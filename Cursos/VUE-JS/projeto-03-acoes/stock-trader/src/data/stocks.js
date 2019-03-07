52ee9dc53c47c66f423e0a2e7708e20cd2cbdd5e
caiooliveira54 <cdom.snf16@uea.edu.br>
2019-03-06 22:22:20 -0400

    VUE-JS: stock-trader

---
+++ Cursos/VUE-JS/projeto-03-acoes/stock-trader/src/data/stocks.js
@@ -0,0 +1,7 @@
+export default [
+    { id: 1, name: 'Playstation 4', price: 150},
+    { id: 1, name: 'Xbox One', price: 100},
+    { id: 1, name: 'PC Alienware', price: 180},
+    { id: 1, name: 'Nintendo Switch', price: 250},
+    { id: 1, name: 'Soulja-Boy Console', price: 30},
+]
